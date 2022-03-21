a8d81fe77b9b131a72e65377ce2dbf61f5449d88
nimatuladhima <92084080+nimatuladhima@users.noreply.github.com>
2022-03-21 16:41:28 +0700

    Add files via upload

--- NOMER3/index.php
+++ NOMER3/index.php
@@ -1,138 +1,72 @@
-<?php 
-include "config.php";
-?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <title>Combobox Bertingkat</title>
+  <title>Mari Belajar Coding - Wilayah Adminstrasi Indonesia</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="asset/select2-4.0.6-rc.1/dist/css/select2.min.css">
+  <script src="asset/jquery/jquery-3.3.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="asset/select2-4.0.6-rc.1/dist/js/select2.min.js"></script>
+  <script src="asset/select2-4.0.6-rc.1/dist/js/i18n/id.js"></script>
+  <script src="asset/js/app.js"></script>
+  <?php
+  include("koneksi.php");
+  ?>
 </head>
 
 <body>
-
-    <div class="container-fluid">
-        <div class=" row justify-content-md-center">
-
-            <div class="col-md-6">
-                <br>
-                <div class="card" style="width: 100%;">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Combobox Bertingkat</h5>
-                        <h6 class="card-subtitle mb-2 text-muted text-center">ClasCode | iniilmu.com</h6>
-                        <form>
-                            <div class="form-group">
-                                <label for="exampleFormControlSelect1">Provinsi</label>
-                                <select class="form-control" id="id_provinsi">
-                                    <option>---Pilih Provinsi---</option>
-                                    <?php
-                                        $sql = mysqli_query($con,"SELECT * FROM provinces order by name ASC") or die(mysqli_error($con));
-                                        while ($dt = mysqli_fetch_array($sql)) {
-                                    ?>
-                                        <option value="<?php echo $dt['id'] ?>"><?php echo $dt['name'] ?></option>
-                                    <?php } ?>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleFormControlSelect1">Kabupaten</label>
-                                <select class="form-control" id="id_kabupaten">
-                                    <option>---Pilih Kabupaten---</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleFormControlSelect1">Kecamatan</label>
-                                <select class="form-control" id="id_kecamatan">
-                                    <option>---Pilih Kecamatan---</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleFormControlSelect1">Kelurhan/Desa</label>
-                                <select class="form-control" id="id_kelurahan">
-                                    <option>---Pilih Kelurahan---</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <button class="btn btn-primary">Submit</button>
-                            </div>
-                        </form>
-                    </div>
+  <div class="container">
+    <br>
+    <div class="row mt-5 pt-5 justify-content-center">
+      <div class="col-md-8 col-sm-8">
+        <div class="card">
+          <div class="card-header"><b>ComboBox Bertingkat</b></div>
+          <div class="card-body">
+            <form class="form-horizontal" method="post">
+              <div class="input-group">
+                <label class="control-label col-sm-3">Provinsi:</label>
+                <div class="col-sm-9">
+                  <?php
+                  $sql_provinsi = mysqli_query($con, "SELECT * FROM provinsi ORDER BY name ASC");
+                  ?>
+                  <select class="form-control" name="" id="provinsi">
+                    <option></option>
+                    <?php
+                    while ($rs_provinsi = mysqli_fetch_assoc($sql_provinsi)) {
+                      echo '<option value="' . $rs_provinsi['id'] . '">' . $rs_provinsi['name'] . '</option>';
+                    }
+                    ?>
+                  </select>
                 </div>
-
-            </div>
-
+              </div>
+              <div class="input-group">
+                <label class="control-label col-sm-3">Kota/Kabupaten:</label>
+                <div class="col-sm-9">
+                  <select class="form-control" name="" id="kota">
+                    <option></option>
+                  </select>
+                </div>
+              </div>
+              <div class="input-group">
+                <label class="control-label col-sm-3">Kecamatan:</label>
+                <div class="col-sm-9">
+                  <select class="form-control" name="" id="kecamatan">
+                    <option></option>
+                  </select>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
-    <script>
-        $(document).ready(function(){
-            $('#id_provinsi').on('change',function(){
-                var id_provinsi = $(this).val();
-                $.ajax({
-                    url:'ambil_data.php',
-                    type:"POST",
-                    data:{
-                        modul:'Kabupaten',
-                        id:id_provinsi
-                    },
-                    success:function(respond){
-                        $("#id_kabupaten").html(respond);
-                    },
-                    error:function(){
-                        alert("Gagal Mengambil Data");
-                    }
-                })
-            })
-
-            $('#id_kabupaten').on('change',function(){
-                var id_provinsi = $(this).val();
-                $.ajax({
-                    url:'ambil_data.php',
-                    type:"POST",
-                    data:{
-                        modul:'Kecamatan',
-                        id:id_provinsi
-                    },
-                    success:function(respond){
-                        $("#id_kecamatan").html(respond);
-                    },
-                    error:function(){
-                        alert("Gagal Mengambil Data");
-                    }
-                })
-            })
-
-            $('#id_kecamatan').on('change',function(){
-                var id_provinsi = $(this).val();
-                $.ajax({
-                    url:'ambil_data.php',
-                    type:"POST",
-                    data:{
-                        modul:'Kelurahan',
-                        id:id_provinsi
-                    },
-                    success:function(respond){
-                        $("#id_kelurahan").html(respond);
-                    },
-                    error:function(){
-                        alert("Gagal Mengambil Data");
-                    }
-                })
-            })
-        });
-
-    </script>
-
+  </div>
+  <footer class="bg-dark text-white text-center pb-3 fixed-bottom">
+    <p>created with Love by <a href="" class="fw-bold text-white">Ahmad Fikri Husaini</a></p>
+  </footer>
 </body>
 
 </html>
